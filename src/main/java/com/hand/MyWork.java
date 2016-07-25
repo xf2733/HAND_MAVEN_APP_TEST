094caeb7c92675c05b358ebaa1ae80af1ccc59d8
Steven Song <haicheng.song@hand-china.com>
2016-07-25 18:55:00 +0800

    Updating my work.

---
+++ src/main/java/com/hand/MyWork.java
@@ -0,0 +1,10 @@
+package com.hand;
+
+public class MyWork {
+
+	public static void main(String[] args) {
+		System.out.println("What's the answer of 1+1?");
+		System.out.println("MY ARSE");
+	}
+
+}
